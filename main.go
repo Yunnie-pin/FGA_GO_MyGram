82a215dae081f0eb2a9fcccdbd2ca2ad207f4f9c
Arifin Yunianta <72777947+Yunnie-pin@users.noreply.github.com>
2024-03-20 12:59:06 +0700

    fix .env curut

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"mygram/helpers"
+	"log"
 	"mygram/router"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	r := router.StartApp()
-	env := helpers.GetEnv("ENV")
-	port := helpers.GetEnv("PORT")
 
-	// if env == "development" {
-	// 	log.Println("Server running on port :8080")
-	// 	r.Run(":8080")
-	// } else if env == "production" {
-	// 	log.Println("Server running on port :80")
-	// 	r.Run(":80")
-	// }
-
-	fmt.Println("ENV: ", env)
-	fmt.Println("PORT: ", port)
-	r.Run(":" + port)
+	log.Println("ENV: ", os.Getenv("ENVIRONMENT_VARIABEL"))
+	log.Println("PORT: ", os.Getenv("PORT"))
+	r.Run(":" + os.Getenv("PORT"))
 }
