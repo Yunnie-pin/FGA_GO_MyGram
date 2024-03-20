652131a0f43dcc218148fde7f61548eb0201af64
Arifin Yunianta <72777947+Yunnie-pin@users.noreply.github.com>
2024-03-20 11:41:41 +0700

    debug railway

--- main.go
+++ main.go
@@ -10,11 +10,15 @@ func main() {
 	r := router.StartApp()
 	env := os.Getenv("ENV")
 
-	if env == "development" {
-		log.Println("Server running on port :8080")
-		r.Run(":8080")
-	} else if env == "production" {
-		log.Println("Server running on port :80")
-		r.Run(":80")
-	}
+	// if env == "development" {
+	// 	log.Println("Server running on port :8080")
+	// 	r.Run(":8080")
+	// } else if env == "production" {
+	// 	log.Println("Server running on port :80")
+	// 	r.Run(":80")
+	// }
+
+	log.Print("ENV: ", env)
+	log.Println("Server running on port :8080")
+	r.Run(":8080")
 }
