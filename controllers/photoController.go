3e3d98dfa788d97700780869f7cb7aca8c8344e7
Arifin Yunianta <72777947+Yunnie-pin@users.noreply.github.com>
2024-03-23 00:09:59 +0700

    fix(photo) : memperbaharui target id

--- controllers/photoController.go
+++ controllers/photoController.go
@@ -35,7 +35,7 @@ func PhotoGet(c *gin.Context) {
 	Photos := []models.Photo{}
 	userID := uint(userData["id"].(float64))
 
-	err := db.Debug().Model(&models.Photo{}).Where("id = ?", userID).Find(&Photos).Error
+	err := db.Debug().Model(&models.Photo{}).Where("user_id = ?", userID).Find(&Photos).Error
 
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
